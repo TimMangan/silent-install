c54a733151fd017b5223a2c79f5565fb11a66dc3
Tim Mangan <tim@tmurgent.onmicrosoft.com>
2017-05-28 11:27:36 -0400

    Fix import issues

--- SilentInstall_Utilities.psm1
+++ SilentInstall_Utilities.psm1
@@ -272,22 +272,22 @@ Function Move-Key {
 .EXAMPLE
     Move-Key 'HKLM' 'Software\Wow6432Node\Microsoft\Internet Explorer\Extensions' '{29e5421f-6f05-4a76-938f-d7e5884f23d8}' 'HKCU'
 #>
-  [CmdletBinding()]�
-  param(�
-����[Parameter(Mandatory=$True,��Position=0)]���
-����[string]$FromHive,
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory=$True, Position=0)]
+    [string]$FromHive,
     
     [Parameter(Mandatory=$True, Position=1)]
     [string]$ParentKey,
     
-����[Parameter(Mandatory=$True,��Position=2)]���
-����[string]$RelativeKey,
+    [Parameter(Mandatory=$True, Position=2)]
+    [string]$RelativeKey,
     
     [Parameter(Mandatory=$True, Position=3)]
     [string]$ToHive
   )
   Process {
-LogMe_AndDisplay "Move-Key: $FromHive $ParentKey $RelativeKey $ToHive"  $InstallerLogFile
+    LogMe_AndDisplay "Move-Key: $FromHive $ParentKey $RelativeKey $ToHive"  $InstallerLogFile
 
     if (!($FromHive -eq 'HKLM' -or $FromHive -eq 'HKCU' ))
     {
@@ -326,157 +326,158 @@ LogMe_AndDisplay "Move-Key: $FromHive $ParentKey $RelativeKey $ToHive"  $Install
     Remove-Item -Path "$FullFrom" -Recurse
 #LogMe_AndDisplay "done moving the key" $InstallerLogFile
   }
-}    �
+}
+
 ############################################################################# 
-Function�New-Shortcut�{�
-<#���
-.SYNOPSIS���
-����This�script�is�used�to�create�a��shortcut.���������
-.DESCRIPTION���
-����This�script�uses�a�Com�Object�to�create�a�shortcut.�
-.PARAMETER�Path�
-����The�path�to�the�shortcut�file.��.lnk�will�be�appended�if�not�specified.��If�the�folder�name�doesn't�exist,�it�will�be�created.�
-.PARAMETER�TargetPath�
-����Full�path�of�the�target�executable�or�file.�
-.PARAMETER�Arguments�
-����Arguments�for�the�executable�or�file.�
-.PARAMETER�Description�
-����Description�of�the�shortcut.�
-.PARAMETER�HotKey�
-����Hotkey�combination�for�the�shortcut.��Valid�values�are�SHIFT+F7,�ALT+CTRL+9,�etc.��An�invalid�entry�will�cause�the��
-����function�to�fail.�
-.PARAMETER�WorkDir�
-����Working�directory�of�the�application.��An�invalid�directory�can�be�specified,�but�invoking�the�application�from�the��
-����shortcut�could�fail.�
-.PARAMETER�WindowStyle�
-����Windows�style�of�the�application,�Normal�(1),�Maximized�(3),�or�Minimized�(7).��Invalid�entries�will�result�in�Normal�
-����behavior.�
-.PARAMETER�Icon�
-����Full�path�of�the�icon�file.��Executables,�DLLs,�etc�with�multiple�icons�need�the�number�of�the�icon�to�be�specified,��
-����otherwise�the�first�icon�will�be�used,�i.e.:��c:\windows\system32\shell32.dll,99�
-.PARAMETER�admin�
-����Used�to�create�a�shortcut�that�prompts�for�admin�credentials�when�invoked,�equivalent�to�specifying�runas.�
-.NOTES���
-����Author��������:�Rhys�Edwards�
-����Email��������:�powershell@nolimit.to���
-.INPUTS�
-����Strings�and�Integer�
-.OUTPUTS�
-����True�or�False,�and�a�shortcut�
-.LINK���
-����Adapted from https://gallery.technet.microsoft.com/scriptcenter/New-Shortcut-4d6fb3d8��
-.EXAMPLE���
-����New-Shortcut�-Path�c:\temp\notepad.lnk�-TargetPath�c:\windows\notepad.exe�����
-����Creates�a�simple�shortcut�to�Notepad�at�c:\temp\notepad.lnk�
-.EXAMPLE�
-����New-Shortcut�"$($env:Public)\Desktop\Notepad"�c:\windows\notepad.exe�-WindowStyle�3�-admin�
-����Creates�a�shortcut�named�Notepad.lnk�on�the�Public�desktop�to�notepad.exe�that�launches�maximized�after�prompting�for��
-����admin�credentials.�
-.EXAMPLE�
-����New-Shortcut�"$($env:USERPROFILE)\Desktop\Notepad.lnk"�c:\windows\notepad.exe�-icon�"c:\windows\system32\shell32.dll,99"�
-����Creates�a�shortcut�named�Notepad.lnk�on�the�user's�desktop�to�notepad.exe�that�has�a�pointy�finger�icon�(on�Windows�7).�
-.EXAMPLE�
-����New-Shortcut�"$($env:USERPROFILE)\Desktop\Notepad.lnk"�c:\windows\notepad.exe�C:\instructions.txt�
-����Creates�a�shortcut�named�Notepad.lnk�on�the�user's�desktop�to�notepad.exe�that�opens�C:\instructions.txt��
-.EXAMPLE�
-����New-Shortcut�"$($env:USERPROFILE)\Desktop\ADUC"�%SystemRoot%\system32\dsa.msc�-admin��
-����Creates�a�shortcut�named�ADUC.lnk�on�the�user's�desktop�to�Active�Directory�Users�and�Computers�that�launches�after��
-����prompting�for�admin�credentials�
-#>�
-�
-[CmdletBinding()]�
-param(�
-����[Parameter(Mandatory=$True,��ValueFromPipelineByPropertyName=$True,Position=0)]��
-����[Alias("File","Shortcut")]��
-����[string]$Path,�
-�
-����[Parameter(Mandatory=$True,��ValueFromPipelineByPropertyName=$True,Position=1)]��
-����[Alias("Target")]��
-����[string]$TargetPath,�
-�
-����[Parameter(ValueFromPipelineByPropertyName=$True,Position=2)]��
-����[Alias("Args","Argument")]��
-����[string]$Arguments,�
-�
-����[Parameter(ValueFromPipelineByPropertyName=$True,Position=3)]���
-����[Alias("Desc")]�
-����[string]$Description,�
-�
-����[Parameter(ValueFromPipelineByPropertyName=$True,Position=4)]���
-����[string]$HotKey,�
-�
-����[Parameter(ValueFromPipelineByPropertyName=$True,Position=5)]���
-����[Alias("WorkingDirectory","WorkingDir")]�
-����[string]$WorkDir,�
-�
-����[Parameter(ValueFromPipelineByPropertyName=$True,Position=6)]���
-����[int]$WindowStyle,�
-�
-����[Parameter(ValueFromPipelineByPropertyName=$True,Position=7)]���
-����[string]$Icon,�
-�
-����[Parameter(ValueFromPipelineByPropertyName=$True)]���
-����[switch]$admin�
-)�
-�
-Process�{�
-�
-��If�(!($Path�-match�"^.*(\.lnk)$"))�{�
-����$Path�=�"$Path`.lnk"�
-��}�
-��[System.IO.FileInfo]$Path�=�$Path�
-��Try�{�
-����If�(!(Test-Path�$Path.DirectoryName))�{�
-������md�$Path.DirectoryName�-ErrorAction�Stop�|�Out-Null�
-����}�
-��}�Catch�{�
-����Write-Verbose�"Unable�to�create�$($Path.DirectoryName),�shortcut�cannot�be�created"�
-����Return�$false�
-����Break�
-��}�
-�
-�
-��#�Define�Shortcut�Properties�
-��$WshShell�=�New-Object�-ComObject�WScript.Shell�
-��$Shortcut�=�$WshShell.CreateShortcut($Path.FullName)�
-��$Shortcut.TargetPath�=�$TargetPath�
-��$Shortcut.Arguments�=�$Arguments�
-��$Shortcut.Description�=�$Description�
-��$Shortcut.HotKey�=�$HotKey�
-��$Shortcut.WorkingDirectory�=�$WorkDir�
-��$Shortcut.WindowStyle�=�$WindowStyle�
-��If�($Icon){�
-����$Shortcut.IconLocation�=�$Icon�
-��}�
-�
-��Try�{�
-����#�Create�Shortcut�
-����$Shortcut.Save()�
-����#�Set�Shortcut�to�Run�Elevated�
-����If�($admin)�{������
-������$TempFileName�=�[IO.Path]::GetRandomFileName()�
-������$TempFile�=�[IO.FileInfo][IO.Path]::Combine($Path.Directory,�$TempFileName)�
-������$Writer�=�New-Object�System.IO.FileStream�$TempFile,�([System.IO.FileMode]::Create)�
-������$Reader�=�$Path.OpenRead()�
-������While�($Reader.Position�-lt�$Reader.Length)�{�
-��������$Byte�=�$Reader.ReadByte()�
-��������If�($Reader.Position�-eq�22)�{$Byte�=�34}�
-��������$Writer.WriteByte($Byte)�
-������}�
-������$Reader.Close()�
-������$Writer.Close()�
-������$Path.Delete()�
-������Rename-Item�-Path�$TempFile�-NewName�$Path.Name�|�Out-Null�
-����}�
-����Return�$True�
-��}�Catch�{�
-����Write-Verbose�"Unable�to�create�$($Path.FullName)"�
-����Write-Verbose�$Error[0].Exception.Message�
-����Return�$False�
-��}�
-�
-}�
-}�
+Function New-Shortcut {
+<#
+.SYNOPSIS
+    This script is used to create a shortcut.
+.DESCRIPTION
+    This script uses a Com Object to create a shortcut.
+.PARAMETER Path
+    The path to the shortcut file.  .lnk will be appended if not specified.  If the folder name doesn't exist, it will be created.
+.PARAMETER TargetPath
+    Full path of the target executable or file. 
+.PARAMETER Arguments
+    Arguments for the executable or file. 
+.PARAMETER Description
+    Description of the shortcut. 
+.PARAMETER HotKey
+    Hotkey combination for the shortcut.  Valid values are SHIFT+F7, ALT+CTRL+9, etc.  An invalid entry will cause the  
+    function to fail. 
+.PARAMETER WorkDir
+    Working directory of the application.  An invalid directory can be specified, but invoking the application from the  
+    shortcut could fail. 
+.PARAMETER WindowStyle
+    Windows style of the application, Normal (1), Maximized (3), or Minimized (7).  Invalid entries will result in Normal 
+    behavior. 
+.PARAMETER Icon
+    Full path of the icon file.  Executables, DLLs, etc with multiple icons need the number of the icon to be specified,  
+    otherwise the first icon will be used, i.e.:  c:\windows\system32\shell32.dll,99 
+.PARAMETER admin 
+    Used to create a shortcut that prompts for admin credentials when invoked, equivalent to specifying runas. 
+.NOTES
+    Author       : Rhys Edwards 
+    Email        : powershell@nolimit.to   
+.INPUTS
+    Strings and Integer 
+.OUTPUTS
+    True or False, and a shortcut 
+.LINK
+    Adapted from https://gallery.technet.microsoft.com/scriptcenter/New-Shortcut-4d6fb3d8  
+.EXAMPLE
+    New-Shortcut -Path c:\temp\notepad.lnk -TargetPath c:\windows\notepad.exe     
+    Creates a simple shortcut to Notepad at c:\temp\notepad.lnk 
+.EXAMPLE
+    New-Shortcut "$($env:Public)\Desktop\Notepad" c:\windows\notepad.exe -WindowStyle 3 -admin 
+    Creates a shortcut named Notepad.lnk on the Public desktop to notepad.exe that launches maximized after prompting for  
+    admin credentials. 
+.EXAMPLE
+    New-Shortcut "$($env:USERPROFILE)\Desktop\Notepad.lnk" c:\windows\notepad.exe -icon "c:\windows\system32\shell32.dll,99" 
+    Creates a shortcut named Notepad.lnk on the user's desktop to notepad.exe that has a pointy finger icon (on Windows 7). 
+.EXAMPLE
+    New-Shortcut "$($env:USERPROFILE)\Desktop\Notepad.lnk" c:\windows\notepad.exe C:\instructions.txt 
+    Creates a shortcut named Notepad.lnk on the user's desktop to notepad.exe that opens C:\instructions.txt  
+.EXAMPLE
+    New-Shortcut "$($env:USERPROFILE)\Desktop\ADUC" %SystemRoot%\system32\dsa.msc -admin  
+    Creates a shortcut named ADUC.lnk on the user's desktop to Active Directory Users and Computers that launches after  
+    prompting for admin credentials 
+#>
+
+[CmdletBinding()] 
+param( 
+    [Parameter(Mandatory=$True,  ValueFromPipelineByPropertyName=$True,Position=0)]  
+    [Alias("File","Shortcut")]  
+    [string]$Path, 
+ 
+    [Parameter(Mandatory=$True,  ValueFromPipelineByPropertyName=$True,Position=1)]  
+    [Alias("Target")]  
+    [string]$TargetPath, 
+ 
+    [Parameter(ValueFromPipelineByPropertyName=$True,Position=2)]  
+    [Alias("Args","Argument")]  
+    [string]$Arguments, 
+ 
+    [Parameter(ValueFromPipelineByPropertyName=$True,Position=3)]   
+    [Alias("Desc")] 
+    [string]$Description, 
+ 
+    [Parameter(ValueFromPipelineByPropertyName=$True,Position=4)]   
+    [string]$HotKey, 
+ 
+    [Parameter(ValueFromPipelineByPropertyName=$True,Position=5)]   
+    [Alias("WorkingDirectory","WorkingDir")] 
+    [string]$WorkDir, 
+ 
+    [Parameter(ValueFromPipelineByPropertyName=$True,Position=6)]   
+    [int]$WindowStyle, 
+ 
+    [Parameter(ValueFromPipelineByPropertyName=$True,Position=7)]   
+    [string]$Icon, 
+ 
+    [Parameter(ValueFromPipelineByPropertyName=$True)]   
+    [switch]$admin 
+) 
+ 
+Process { 
+ 
+  If (!($Path -match "^.*(\.lnk)$")) { 
+    $Path = "$Path`.lnk" 
+  } 
+  [System.IO.FileInfo]$Path = $Path 
+  Try { 
+    If (!(Test-Path $Path.DirectoryName)) { 
+      md $Path.DirectoryName -ErrorAction Stop | Out-Null 
+    } 
+  } Catch { 
+    Write-Verbose "Unable to create $($Path.DirectoryName), shortcut cannot be created" 
+    Return $false 
+    Break 
+  } 
+ 
+ 
+  # Define Shortcut Properties 
+  $WshShell = New-Object -ComObject WScript.Shell 
+  $Shortcut = $WshShell.CreateShortcut($Path.FullName) 
+  $Shortcut.TargetPath = $TargetPath 
+  $Shortcut.Arguments = $Arguments 
+  $Shortcut.Description = $Description 
+  $Shortcut.HotKey = $HotKey 
+  $Shortcut.WorkingDirectory = $WorkDir 
+  $Shortcut.WindowStyle = $WindowStyle 
+  If ($Icon){ 
+    $Shortcut.IconLocation = $Icon 
+  } 
+ 
+  Try { 
+    # Create Shortcut 
+    $Shortcut.Save() 
+    # Set Shortcut to Run Elevated 
+    If ($admin) {      
+      $TempFileName = [IO.Path]::GetRandomFileName() 
+      $TempFile = [IO.FileInfo][IO.Path]::Combine($Path.Directory, $TempFileName) 
+      $Writer = New-Object System.IO.FileStream $TempFile, ([System.IO.FileMode]::Create) 
+      $Reader = $Path.OpenRead() 
+      While ($Reader.Position -lt $Reader.Length) { 
+        $Byte = $Reader.ReadByte() 
+        If ($Reader.Position -eq 22) {$Byte = 34} 
+        $Writer.WriteByte($Byte) 
+      } 
+      $Reader.Close() 
+      $Writer.Close() 
+      $Path.Delete() 
+      Rename-Item -Path $TempFile -NewName $Path.Name | Out-Null 
+    } 
+    Return $True 
+  } Catch { 
+    Write-Verbose "Unable to create $($Path.FullName)" 
+    Write-Verbose $Error[0].Exception.Message 
+    Return $False 
+  } 
+ 
+} 
+} 
 
 
 
@@ -1046,30 +1047,30 @@ Function Flush_NGensQueues([string]$InstallerLogFile)
 }
 
 
-function�Set_PSWinSize([int]$MaxWidth,[int]$MaxHeight) �
-{�
-�����
-����if�($Host.Name�-match�"console")�
-����{�
-�
-��������##$MyBuffer�=�$Host.UI.RawUI.BufferSize�
-��������$MyWindowSize�=�$Host.UI.RawUI.WindowSize�
+function Set_PSWinSize([int]$MaxWidth,[int]$MaxHeight)  
+{ 
+     
+    if ($Host.Name -match "console") 
+    { 
+ 
+        ##$MyBuffer = $Host.UI.RawUI.BufferSize 
+        $MyWindowSize = $Host.UI.RawUI.WindowSize 
         $MyWindowPos = $Host.UI.RawUI.WindowPosition
-�����
-��������$MyWindowSize.Height�=�($MaxHeight)�
-��������$MyWindowSize.Width�=�($Maxwidth)
+     
+        $MyWindowSize.Height = ($MaxHeight) 
+        $MyWindowSize.Width = ($Maxwidth)
         $MyWindowPos.X = 5
         $MyWindowPos.Y = 5
 
-�
-��������##$MyBuffer.Height�=�(9999)�
-��������##$MyBuffer.Width�=�($MaxWidth)�
-�
-��������##$host.UI.RawUI.set_bufferSize($MyBuffer)�
-��������$host.UI.RawUI.set_windowSize($MyWindowSize)
-        $host.UI.RawUI.set_windowPosition($MyWindowPos)�
-����}�
-�
+ 
+        ##$MyBuffer.Height = (9999) 
+        ##$MyBuffer.Width = ($MaxWidth) 
+ 
+        ##$host.UI.RawUI.set_bufferSize($MyBuffer) 
+        $host.UI.RawUI.set_windowSize($MyWindowSize)
+        $host.UI.RawUI.set_windowPosition($MyWindowPos) 
+    } 
+ 
 }
 
 function Set_PSWinColors([string]$Background,[string]$Foreground,[string]$WinTitle,[bool]$Elevated)
@@ -1078,15 +1079,15 @@ function Set_PSWinColors([string]$Background,[string]$Foreground,[string]$WinTit
     $host.ui.RawUI.ForegroundColor = $Foreground
     Clear-Host
 
-����If�($Elevated)
+    If ($Elevated)
     {
-        $Elevatedstr�=�"[Elevated]"
+        $Elevatedstr = "[Elevated]"
     }
     else
     {
-        $Elevatedstr = ""���
-����}
-����$Title�=�$Elevatedstr�+�"�$ENV:USERNAME".ToUpper()�+�":�$($Host.Name)�"�+�"�-�"�+�(Get-Date).toshortdatestring() + $WinTitle��
-����$Host.UI.RawUI.set_WindowTitle($Title)�
+        $Elevatedstr = ""   
+    }
+    $Title = $Elevatedstr + " $ENV:USERNAME".ToUpper() + ": $($Host.Name) " + " - " + (Get-Date).toshortdatestring() + $WinTitle  
+    $Host.UI.RawUI.set_WindowTitle($Title) 
 
-}�
+} 
