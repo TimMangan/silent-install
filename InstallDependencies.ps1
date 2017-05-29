4d046f4c2adc80eba8bc32310098201d0bb6eb38
Tim Mangan <tim@tmurgent.onmicrosoft.com>
2017-05-29 15:17:52 -0400

    Sync to changes in utilities module.

--- InstallDependencies.ps1
+++ InstallDependencies.ps1
@@ -7,14 +7,15 @@
 # Get folder that this PS1 file is in so that we can find files correctly from relative references
 $executingScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 # Bring in common utility code
-Import-Module $executingScriptDirectory\SilentInstall_Utilities.ps1
+Import-Module $executingScriptDirectory\SilentInstall_Utilities.psm1
 
-Set_PSWinSize 80 48
-Set_PSWinColors 'DarkGray' 'White' 'PowerShell - InstallDependencies.ps1' $false 
+Set_PSWinSize 80 48 5 5
+Set_PSWinColors 'Black' 'White' 'PowerShell - InstallDependencies.ps1' $false 
 
 # Ensure we are running elevated
-SilentInstall-EnsureElevated $PSCommandPath
+SilentInstall_EnsureElevated $PSCommandPath
 
+Set_PSWinSize 80 48 10 10
 Set_PSWinColors 'DarkGray' 'White' 'PowerShell - InstallDependencies.ps1' $true 
 
 Write-host 'Starting - InstallDependencies.ps1'
@@ -33,7 +34,7 @@ Write-host 'Starting - InstallDependencies.ps1'
 ##########
 # Pre installation Ngen Flushing
 Write-Host "Flushing NGEN Queues"
-SilentInstall-FlushNGensQueues
+SilentInstall_FlushNGensQueues
 write-host -ForegroundColor "Green"  "Done."
-Sleep 5
+Start-Sleep 5
 
